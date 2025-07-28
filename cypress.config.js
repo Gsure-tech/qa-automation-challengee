ddc10f62a1d70fe92ad16c9a26c50569b4b4ec52
gsure-tech <Geeescode@>
2025-07-28 08:26:16 +0100

    fix: update ci

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:3001",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
